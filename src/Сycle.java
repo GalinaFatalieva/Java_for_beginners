076880c15144935df831103671ff924e60fd7963
GalinaFatalieva <68509542+GalinaFatalieva@users.noreply.github.com>
2020-08-30 19:41:56 +0300

    Задача №8: Цикличный вывод данных на экран

---
+++ src/Сycle.java
@@ -0,0 +1,23 @@
+public class Сycle {
+
+    // Необходимо с помощью цикла вывести на экран числа от 1 до 50.
+
+    static void cycleFor(){
+
+        for (int i = 1; i <= 50; i++){
+            System.out.println(i);
+        }
+    }
+
+    static void cycleWhile(){
+
+        int y = 1;
+
+        while (y <=50){
+
+            System.out.println(y);
+            y++;
+        }
+    }
+
+}
