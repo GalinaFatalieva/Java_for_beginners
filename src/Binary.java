3dd12555b75190e19438600d648af78e6113d523
GalinaFatalieva <68509542+GalinaFatalieva@users.noreply.github.com>
2020-08-30 11:43:15 +0300

    Задача №4: Отработка поразрядных операций

---
+++ src/Binary.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Binary {
+
+    //Ввести с консоли число в бинарном формате. Перевести его в десятичный формат, записать в переменную int и вывести на экран.
+
+    static String enter(){
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter a number in binary format: ");
+        String str = in.nextLine();
+
+
+        return str;
+    }
+
+    static void bin(){
+
+        String str = enter();
+
+        int bin = Integer.parseInt(str, 2);
+        System.out.println("Your number Integer: " + bin);
+
+    }
+
+
+
+
+
+}
