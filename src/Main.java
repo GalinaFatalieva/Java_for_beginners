7798c04b3a55884dc74b56b639b8affb8a106d3a
GalinaFatalieva <68509542+GalinaFatalieva@users.noreply.github.com>
2020-08-31 21:27:09 +0300

    Задание 14 обработка исключений в class TypeСonversion

--- src/Main.java
+++ src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        try {
-            TypeСonversion.conversion();
-        } catch (NumberFormatException exc){
-            System.err.println("Введите число без дробной части");
-        }
+       TypeСonversion.conversion();
 
     }
 }
